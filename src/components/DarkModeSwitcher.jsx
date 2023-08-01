c2eadb2fd6b9e3c55f0b4753e016877398439d40
ana117 <63540951+ana117@users.noreply.github.com>
2023-08-01 18:42:23 +0700

    Store preferred theme on local storage

--- src/components/DarkModeSwitcher.jsx
+++ src/components/DarkModeSwitcher.jsx
@@ -1,17 +1,32 @@
 import {BiSolidMoon, BiSolidSun, BiSolidCircle} from 'react-icons/bi';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const DarkModeSwitcher = () => {
     const [darkMode, setDarkMode] = useState(true);
 
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
+    useEffect(() => {
+        const isUsingDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+        if (isUsingDarkMode === null) {
+            localStorage.setItem('darkMode', JSON.stringify(true));
+        } else {
+            enableDarkMode(isUsingDarkMode);
+        }
+    }, [])
+
+    const enableDarkMode = (enable) => {
+        setDarkMode(enable);
+        localStorage.setItem('darkMode', JSON.stringify(enable));
+
         const root = window.document.documentElement;
-        root.classList.toggle('dark');
+        if (enable) {
+            root.classList.add('dark');
+        } else {
+            root.classList.remove('dark');
+        }
     }
 
     return (
-        <button onClick={toggleDarkMode} aria-label="Toggle Dark Mode"
+        <button onClick={() => {enableDarkMode(!darkMode)}} aria-label="Toggle Dark Mode"
                 className="flex gap-[1rem] p-[0.25rem] rounded-full
                            bg-background-dark dark:bg-background
                            text-4xl text-text-dark dark:text-text">
