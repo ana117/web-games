97baec4b3ccfc217e35bd935cf586598f39c43ab
ana117 <63540951+ana117@users.noreply.github.com>
2023-08-01 20:19:44 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import {useEffect} from "react";
 import GameHubPage from "./Hub";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-  useEffect(() => {
-    document.title = "Game Hub"
-  }, []);
+    useEffect(() => {
+        document.title = "Game Hub"
+    }, []);
 
-  return (
-    <div className="m-0 bg-background dark:bg-background-dark text-text dark:text-text-dark
+    return (
+        <div className="m-0 bg-background dark:bg-background-dark text-text dark:text-text-dark
                     transition-colors duration-500">
-      <GameHubPage />
-    </div>
-  );
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<GameHubPage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
