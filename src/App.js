aa44f1d8749c0cf4ef42ffe71dba6318d8ab545e
ana117 <63540951+ana117@users.noreply.github.com>
2023-08-01 20:45:57 +0700

    Change to hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useEffect} from "react";
 import GameHubPage from "./Hub";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import ErrorPage from "./Error";
 
 function App() {
@@ -11,12 +11,12 @@ function App() {
     return (
         <div className="m-0 bg-background dark:bg-background-dark text-text dark:text-text-dark
                     transition-colors duration-500">
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<GameHubPage/>}/>
                     <Route path="*" element={<ErrorPage/>}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
