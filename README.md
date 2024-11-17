a9aa33eebbb5382a078fd916d0b4f3d334fb4fd1
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-17 16:14:21 +0700

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Games Collection
+
+This is a collection of simple browser games made using NextJS. They are all playable in the browser.
+
+[Play Now!](https://ana117.github.io/web-games/)
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <h1>
+        Coming soon!
+      </h1>
+    </main>
+  );
+}
