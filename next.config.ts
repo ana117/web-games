6ed4fdd499b9df3002f2a5527a4e25dad033946d
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-17 21:40:01 +0700

    chore: setup deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/web-games",
+  output: "export",
 };
 
 export default nextConfig;
